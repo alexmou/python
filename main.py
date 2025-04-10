954f585ca1dc187af2d409ea4795528159781abb
alexmou <l.murygin@gmail.com>
2025-04-10 09:28:20 +0300

    m mk

--- main.py
+++ main.py
@@ -1,1 +1,14 @@
-vvvvvvvvvvvvvvv
+import os
+from utils.channel_parser import ChannelParser
+
+channels_to_parse = ['vietnamnews_ru', '', '']
+
+for channel in channels_to_parse:
+    parser = ChannelParser(channel, '2025-04-08 00:00:00', '2025-04-09 00:00:00')
+    parser.scrape()
+
+    path_to_save = os.path.abspath(f'./data/{channel}.json')
+    parser.save_json(path_to_save)
+
+
+
