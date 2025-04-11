aa03400f7510d9b643924d1b4aeb2a5065bb5236
alexmou <l.murygin@gmail.com>
2025-04-11 09:08:33 +0300

    2

--- main.py
+++ main.py
@@ -1,13 +1,14 @@
 import os
 from utils.channel_parser import ChannelParser
 
-channels_to_parse = ['vietnamnews_ru', '', '']
+channels_to_parse = ['vietnamnews_ru']
 
 for channel in channels_to_parse:
-    parser = ChannelParser(channel, '2025-04-08 00:00:00', '2025-04-09 00:00:00')
+    parser = ChannelParser(channel, '2025-04-10 00:00:00', None)
     parser.scrape()
 
     path_to_save = os.path.abspath(f'./data/{channel}.json')
+    path_to_save = os.path.abspath('C:/Users/User/IdeaProjects/python/channel.json')
     parser.save_json(path_to_save)
 
 
