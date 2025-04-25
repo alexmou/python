0977a4c97fac865e765af9b434c766d4c45e3fe8
alexmou <l.murygin@gmail.com>
2025-04-25 14:43:34 +0300

    3

--- utils/driver_manager.py
+++ utils/driver_manager.py
@@ -33,21 +33,14 @@ class TelegramDriverManager:
 
         options = Options()
         if self.headless:
-            options.add_argument('--disable-gpu')
-            options.add_argument('--disable-software-rasterizer')
-            options.add_argument('--no-xshm')
-            options.add_argument('--disable-setuid-sandbox')
+            options.add_argument('--no-sandbox')
             options.add_argument('--disable-dev-shm-usage')
-            options.add_argument('--disable-accelerated-2d-canvas')
-            options.add_argument('--disable-features=VizDisplayCompositor')
-            options.add_argument("--headless=new")
-            options.add_argument("--disable-blink-features=AutomationControlled")
-            options.add_argument("--no-sandbox")
-            options.add_argument("--disable-dev-shm-usage")
-            options.add_argument("--start-maximized")
-            options.add_argument("user-agent=Mozilla/5.0")
-            options.add_experimental_option("excludeSwitches", ["enable-automation"])
-            options.add_experimental_option("useAutomationExtension", False)
+            options.add_argument('--disable-gpu')
+            options.add_argument('--window-size=1920,1080')
+
+            # Обход защиты headless
+            options.add_argument('--headless=new')
+            options.add_argument("user-agent=Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36")
 
         if self.user_data_dir:
             os.makedirs(self.user_data_dir, exist_ok=True)
