5c3aba7cf452aa4428914f0a0509c47a8cfb2076
alexmou <l.murygin@gmail.com>
2025-04-25 14:30:18 +0300

    3

--- utils/driver_manager.py
+++ utils/driver_manager.py
@@ -33,14 +33,21 @@ class TelegramDriverManager:
 
         options = Options()
         if self.headless:
+            options.add_argument('--disable-gpu')
+            options.add_argument('--disable-software-rasterizer')
+            options.add_argument('--no-xshm')
+            options.add_argument('--disable-setuid-sandbox')
+            options.add_argument('--disable-dev-shm-usage')
+            options.add_argument('--disable-accelerated-2d-canvas')
+            options.add_argument('--disable-features=VizDisplayCompositor')
             options.add_argument("--headless=new")
-        options.add_argument("--disable-blink-features=AutomationControlled")
-        options.add_argument("--no-sandbox")
-        options.add_argument("--disable-dev-shm-usage")
-        options.add_argument("--start-maximized")
-        options.add_argument("user-agent=Mozilla/5.0")
-        options.add_experimental_option("excludeSwitches", ["enable-automation"])
-        options.add_experimental_option("useAutomationExtension", False)
+            options.add_argument("--disable-blink-features=AutomationControlled")
+            options.add_argument("--no-sandbox")
+            options.add_argument("--disable-dev-shm-usage")
+            options.add_argument("--start-maximized")
+            options.add_argument("user-agent=Mozilla/5.0")
+            options.add_experimental_option("excludeSwitches", ["enable-automation"])
+            options.add_experimental_option("useAutomationExtension", False)
 
         if self.user_data_dir:
             os.makedirs(self.user_data_dir, exist_ok=True)
